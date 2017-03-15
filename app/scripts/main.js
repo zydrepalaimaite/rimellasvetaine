fa8b53a2e95f1273c94fd31cbcf85cac1b285aee
zydrepalaimaite <zydre.palaimaite@gmail.com>
2017-03-15 06:06:46 +0200

    atnaujinimai 03.15 06.00

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,5 @@
 $('.carousel').carousel({
-    interval: 8000 //changes the speed
+    interval: 5000 //changes the speed
 });
 
 
@@ -32,6 +32,6 @@ $(document).ready(function() {
   //Set the carousel options
   $('#quote-carousel').carousel({
     pause: true,
-    interval: 10000,
+    interval: 5000,
   });
 });
